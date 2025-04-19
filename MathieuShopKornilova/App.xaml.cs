ad0992364c0e09c2e3eaa7d065c56de3796791e7
User <User@DESKTOP-D418H11>
2025-04-19 14:22:15 +0300

    Добавьте файлы проекта.

---
+++ MathieuShopKornilova/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MathieuShopKornilova.Base;
+
+namespace MathieuShopKornilova
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MatueShopKornilovaEntities1 db = new MatueShopKornilovaEntities1();
+        public static MainWindow mainWindow;
+    }
+}
---
+++ MathieuShopKornilova/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MathieuShopKornilova.Pages;
+
+namespace MathieuShopKornilova
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            App.mainWindow =this; MainFrame.NavigationService.Navigate(new AutharizationPage());
+        }
+
+        private void MinWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Вы действительно хотите закрыть приложение?",
+                                     "Подтверждение закрытия",
+                                     MessageBoxButton.YesNo,
+                                     MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
