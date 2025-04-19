7fa343982b1505d9807bbf716097ee53124219ce
User <User@DESKTOP-D418H11>
2025-04-19 14:33:41 +0300

    Add Autharization and Registration

--- MathieuShopKornilova/Pages/AutharizationPage.xaml.cs
+++ MathieuShopKornilova/Pages/AutharizationPage.xaml.cs
@@ -24,5 +24,28 @@ namespace MathieuShopKornilova.Pages
         {
             InitializeComponent();
         }
+        private void EnterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTb.Text;
+            string password = PasswordTb.Text;
+
+            var user = App.db.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
+            if (user != null)
+            {
+                MessageBox.Show("Coll");
+                NavigationService.Navigate(new ServicesPage());
+            }
+            else
+            {
+                MessageBox.Show("Wrong");
+
+            }
+
+        }
+
+        private void RegistrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistrationPage());
+        }
     }
 }
---
+++ MathieuShopKornilova/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MathieuShopKornilova.Base;
+
+namespace MathieuShopKornilova.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+        private void RegistrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTb.Text;
+            string password = PasswordTb.Text;
+            string fullName = NameTb.Text;
+
+            if (RegisterUser(login, password, fullName))
+            {
+                MessageBox.Show("Регистрация прошла успешно.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                NavigationService.Navigate(new AutharizationPage());
+            }
+            else
+            {
+                MessageBox.Show("Пользователь с таким логином уже существует.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        private bool RegisterUser(string login, string password, string fullName)
+        {
+
+            var existingUser = App.db.Users.FirstOrDefault(u => u.Login == login);
+            if (existingUser != null)
+                return false;
+
+            var newUser = new Users
+            {
+                Login = login,
+                Password = password,
+                RoleId = 1,
+                NameUser = fullName,
+            };
+
+            App.db.Users.Add(newUser);
+            App.db.SaveChanges();
+
+            return true;
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
